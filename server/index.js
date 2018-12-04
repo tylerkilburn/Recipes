f541c516436b04ba807e8e5ea89bcd029b67df82
Tyler Kilburn <tylerkilburn@carfax.com>
2018-12-03 22:04:21 -0500

    Initial Recipes

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.static('public'))
+app.use('/', express.static('client'))
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
+
